4e6a1fef190da8bbb7c4966817ef707377c79a87
Joseph Smith <jsmith47265@gmail.com>
2021-01-31 19:25:13 -0500

    Added delete confirmation dialog

--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
 import star from 'images/star.png'
 import coin from 'images/coin.png'
 import Player from './Player.jsx'
 import GameHeader from './GameHeader.jsx'
 import {deleteIcon} from 'util.js';
+import ModalDialog from '../ModalDialog';
 
 
 
@@ -11,6 +12,8 @@ function Game({game, getGameData}){
 
   //Sort players by place
   const playersSorted = game.players.sort((a, b) => (a.placed > b.placed) ? 1: -1);
+
+  const [showModal, setShowModal] = useState(false);
 
   const deleteGame = () => {
     fetch('http://localhost:8080/api/deletegame', {
@@ -27,10 +30,24 @@ function Game({game, getGameData}){
     })
   }
 
+  // const [isVisible, setVisible] = useState(true);
+  // const domRef = useRef();
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(entries => {
+  //     entries.forEach(entry => setVisible(entry.isIntersecting));
+  //   });
+  //   observer.observe(domRef.current);
+  //   let current = domRef.current;
+  //   return () => observer.unobserve(current);
+  // }, []);
+
 
   return(
+    <div>
+    <ModalDialog showModal={showModal} setShowModal={setShowModal}
+                 deleteGame={deleteGame} modalAction="Delete" />
       <div className="game-div med-blue-border">
-        <GameHeader board={game.board} date={game.date}/>
+        <GameHeader board={game.board} date={new Date(game.date).toDateString()}/>
         <table className="scores-table yellow-header">
           <thead>
             <tr className="scores-headers">
@@ -55,9 +72,10 @@ function Game({game, getGameData}){
           </tbody>
       </table>
       <div className="game-buttons">
-        <button onClick={deleteGame}>
+        <button onClick={() => setShowModal(true)}>
           <img className='game-button-img' src={deleteIcon} alt='Yellow Delete Icon'/>
         </button>
+      </div>
       </div>
     </div>
   );
